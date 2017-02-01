56c837ea3e261fd13d67ccb54e6bb0f2a08ad5b2
Giorgos Zafaras <zafarascsd@gmail.com>
2017-02-01 23:37:20 +0200

    add package test

--- 01_helloWorld/app.go
+++ 01_helloWorld/app.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+
+
 func main() {
-	fmt.Println("Hello Zaf!!!!!!!")
+	fmt.Println(4545)
+
+	for i := 0;i<= 100; i++{
+		fmt.Printf("%d,%b,%#x\n",i,i,i)
+
+	}
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	f,g,h := 47,0.0,"gzaf"
+
+	fmt.Printf("%v\n",a)
+	fmt.Printf("%v\n",b)
+	fmt.Printf("%v\n",c)
+	fmt.Printf("%v\n",d)
+
+	fmt.Printf("%t\n",f)
+	fmt.Printf("%t\n",g)
+	fmt.Printf("%t\n",h)
+
 }
---
+++ 01_helloWorld/test/name.go
@@ -0,0 +1,3 @@
+package test
+
+var myName = "Γιώργος Ζαφάρας"
